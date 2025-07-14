c0c2efc50c8e0190b6483083b0edb86619dbc23d
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2025-07-14 11:57:33 +0000

    Criação do metodo load_Model

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import gdown as gd
+import tensorflow as tf
+
+def load_model():
+    #https://drive.google.com/file/d/1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp/view?usp=sharing
+    url = 'https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp'
+    gd.down(url, 'modelo_quantizado16bits.tflite')
+    interpreter = tf.lite.Interpreter(model_path="modelo_quantizado16bits.tflite")
+    interpreter.alocate_tensors()
+    return interpreter
 
 
 def main():
