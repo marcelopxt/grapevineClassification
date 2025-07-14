29b09cd8224e210d42285450513eddd16f76b018
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2025-07-14 11:43:46 +0000

    "Primeiro Commit"

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Classiica folhas de Videiras!")
+    st.write("# Classifica Folhas de Videiras!")
+    
+    
+if __name__ == "__main__":
+        main()
+
+
